c8dc23c74c97e3fb3047941886f1a58473b0a9a9
Andre309 <andrij.kibish@gmail.com>
2024-05-01 14:22:05 +0300

    conditions and loops

---
+++ conditions and loops/task3.js
@@ -0,0 +1,26 @@
+//Цикл For
+console.log();
+
+const num_for = 3;
+
+console.log(`Таблиця множення для числа ${num_for}:`);
+console.log();
+
+for (let i=1; i <=10; i++) {
+    console.log(`${num_for} * ${i} = ${num_for * i}`);
+}
+
+console.log();
+
+//Цикл While
+
+const num_while = 9;
+let i = 1;
+
+console.log(`Таблиця множення для числа ${num_while}:`);
+console.log();
+
+while (i <= 10) {
+    console.log(`${num_while} * ${i} = ${num_while * i}`);
+    i++;
+}

19a3b4007ffed7a75c1608df5658b5d2a3a93bd7
Andre309 <andrij.kibish@gmail.com>
2024-05-14 15:51:19 +0300

    error.message

---
+++ Advanced function usage/task3.js
@@ -0,0 +1,63 @@
+import chalk from "chalk";
+
+function divide(numerator, denominator) {
+    if (denominator === 0) {
+        throw new Error(chalk.red('Ділення на нуль недопустиме!'));
+    } else if (typeof numerator !== 'number' || typeof denominator !== 'number') {
+        throw new Error(chalk.red('Ділення неможливе. Один з аргументів не є числом.'));
+    } 
+
+    const sum = numerator / denominator
+    console.log(chalk.blue(sum));
+    //return sum;
+}
+
+console.log();
+
+// 1 case
+
+try {
+    divide(20, 5);
+} catch (error) {
+    console.error('Помилка:', error.message);
+} finally {
+    console.log(chalk.gray('Робота завершена'))
+}
+
+console.log();
+
+// 2 case
+
+try {
+    divide(20, 0);
+} catch (error) {
+    console.error('Помилка:', error.message);
+} finally {
+    console.log(chalk.gray('Робота завершена'))
+}
+
+console.log();
+
+// 3 case
+
+try {
+    divide('bb', 5);
+} catch (error) {
+    console.error('Помилка:', error.message);
+} finally {
+    console.log(chalk.gray('Робота завершена'))
+}
+
+console.log();
+
+// 4 case
+
+try {
+    divide(20, true);
+} catch (error) {
+    console.error('Помилка:', error.message);
+} finally {
+    console.log(chalk.gray('Робота завершена'))
+}
+
+console.log();

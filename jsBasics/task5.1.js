85f21ec312e27ef5d2bdf1f1d839ecccdcf16b26
Andre309 <andrij.kibish@gmail.com>
2024-05-14 15:50:25 +0300

    fix error.message

--- jsBasics/task5.1.js
+++
@@ -1,5 +0,0 @@
-let radius = 15;
-
-let square = Math.PI * Math.pow(radius, 2); 
-
-console.log((square).toFixed(2));
--- jsBasics/task5.3.js
+++
@@ -1,7 +0,0 @@
-let radius = 12;
-
-let height = 4;
-
-let volume = Math.PI * Math.pow(radius, 2) * height;
-
-console.log((volume).toFixed(2));

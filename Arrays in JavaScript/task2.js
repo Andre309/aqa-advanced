5c06752b61667359acc8387993476e82c635c6bf
Andre309 <andrij.kibish@gmail.com>
2024-05-19 15:06:28 +0300

    Arrays

---
+++ Arrays in JavaScript/task2.js
@@ -0,0 +1,12 @@
+const arr = [1, 2, 3, 4, 5];
+function doubleArr(nums) {
+    return nums.map((num) => num * 2);
+}
+
+const doubledArr = doubleArr(arr);
+
+console.log();
+
+console.log(doubledArr);
+
+console.log();
---
+++ Arrays in JavaScript/task5.js
@@ -0,0 +1,5 @@
+const firstArray = ['Swiss', 'Croatia', 'Ukraine', 'Israel', 'France'];
+const secondArray = ['1', '2', '3', '4', '5'];
+
+const thirdArray = firstArray.concat(secondArray);
+console.log(thirdArray);
